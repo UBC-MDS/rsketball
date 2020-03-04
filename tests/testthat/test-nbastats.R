82dc54ca3441caddaa98aa5d91312dde2203c3df
V Anand Shankar <vanandshankar@yahoo.com>
2020-03-03 17:53:11 -0800

    Update test-nbastats for dummy testing

--- tests/testthat/test-nbastats.R
+++ tests/testthat/test-nbastats.R
@@ -1,3 +1,39 @@
-test_that("nbastats processes the inputs", {
-  expect_equal(2 * 2, 4)
-})
+#' Generate summary stats for NBA players
+#'
+#' @description
+#' The function filters the dataset further using the arguments provided and
+#' produces a tibble with summary statistics for a list of columns of a few players or teams.
+#' The function can only use one of the two filters - playerNames and teamNames. Hence, If playerNames
+#' are provided, teamNames are ignored.
+#'
+#' @param data tibble
+#' @return A tibble
+#' @export
+#'
+#' @importFrom readr read_csv
+#' @import dplyr
+#'
+#' @examples
+#' test_nbastats(nba_data)
+#'
+test_nbastats <- function(data) {
+
+  test_that("nbastats returns a data frame when all is FALSE", {
+    expect_equal(
+      is.data.frame(nbastats(data, columnNames = c('GP', '3PM', 'FT%'),teamNames = c('UTAH', 'PHX', 'DET'), positions = c('C'), all = FALSE)),
+      TRUE
+    )
+  })
+
+  test_that("nbastats returns a data frame when all iS TRUE", {
+    expect_equal(
+      is.data.frame(nbastats(data, columnNames = c('GP', '3PM', 'FT%'),teamNames = c('UTAH', 'PHX', 'DET'), positions = c('C'), all = TRUE)),
+      TRUE
+    )
+  })
+
+}
+
+
+data = read_csv("https://raw.githubusercontent.com/kfoofw/nba_espn/master/0.data/NBA_reg_2018-2019.csv")
+test_nbastats(data)
